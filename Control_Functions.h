69d11516a2c672599bbca3979a711970637d27e5
ckester99 <CKESTER@MURRAYSTATE.EDU>
2019-11-05 15:53:28 -0600

    Update Control_Functions.h

--- Control_Functions.h
+++ Control_Functions.h
@@ -1,6 +1,18 @@
 # ifndef CONTROL_FUNCTIONS_H
 #define CONTROLS_FUNCTIONS_H
 
-void PID();
+typedef struct vals{
+  float KP;
+  float KI;
+  float KD;
+  uint16_t DT;
+  double goal;
+  
+  double prevError;
+  unsigned long prevTime
+  long double intError;  
+} PIDdata;
+
+double PID(PiDdata *dat,double currValue);
 
 #endif

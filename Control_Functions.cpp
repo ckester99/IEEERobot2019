f829ee692d1ba3a91bab8643e9431495554d60f3
ckester99 <CKESTER@MURRAYSTATE.EDU>
2019-11-07 10:12:21 -0600

    Update Control_Functions.cpp

--- Control_Functions.cpp
+++ Control_Functions.cpp
@@ -1,6 +1,6 @@
 double PID(PIDdata *dat, double currValue){
-  double error = currValue - *dat.goal;
-  *dat.intError += error * *dat.DT;
+  double error = currValue - dat->goal;
+  dat->intError += error * dat->DT;
   
-  return (*dat.KP * error + *dat.KI * *dat.intError + *dat.KD * (error - *dat.prevError)/ *dat.DT);
+  return (dat->KP * error + dat->KI * dat->intError + dat->KD * (error - dat->prevError)/ dat->DT);
 }

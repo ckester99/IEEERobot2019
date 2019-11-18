ff8791e37d79ae73088923b66b2aab384fc8053e
ckester99 <CKESTER@MURRAYSTATE.EDU>
2019-11-18 08:42:30 -0600

    Update Control_Functions.h

--- Control_Functions.h
+++ Control_Functions.h
@@ -5,7 +5,6 @@ typedef struct vals{
   float KP;
   float KI;
   float KD;
-  uint16_t DT;
   double goal;
   
   double prevError;
@@ -13,6 +12,6 @@ typedef struct vals{
   long double intError;  
 } PIDdata;
 
-double PID(PiDdata *dat,double currValue);
+double PID(PiDdata *dat,double currValue, int dt);
 
 #endif

6b04a8d68fe3adfcc5960a773867959fa4bdf959
ckester99 <CKESTER@MURRAYSTATE.EDU>
2020-01-28 09:03:58 -0600

    Delete Control_Functions.cpp

--- Control_Functions.cpp
+++
@@ -1,8 +0,0 @@
-double PID(PIDdata *dat, double currValue, int dt){
-  double error = currValue - dat->goal;
-  dat->intError += error * dt;
-  
-  return (dat->KP * error + dat->KI * dat->intError + dat->KD * (error - dat->prevError)/ dt);
-}
-
-

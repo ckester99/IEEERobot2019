42027a86daa32c1d6728f00dd9102da88aafdc69
ckester99 <CKESTER@MURRAYSTATE.EDU>
2020-02-05 14:09:25 -0600

    Add files via upload

---
+++ Line_Following.ino
@@ -0,0 +1,52 @@
+#define lLightPin 1
+#define rLightPin 1
+#define WHITE_TOL 900
+#define DEB_TIME 25000 //in microseconds
+
+float RPM;
+unsigned long debWhite, debBlack, lastTime;
+int pos, lLight,rLight, travelDir;
+bool onWhite,changePosFlag;
+
+void followLine(unsigned long currTime){
+  lLight = analogRead(lLightPin);
+  rLight = analogRead(rLightPin);
+  unsigned long dt = currTime-lastTime;
+  
+  if ( lLight > WHITE_TOL && rLight > WHITE_TOL){//drive straight
+    debWhite += dt;
+    debBlack =0;
+  }
+
+  if ( lLight < WHITE_TOL && rLight < WHITE_TOL){//drive straight
+    debWhite = 0;
+    debBlack += dt;
+  }
+
+  if ( lLight > WHITE_TOL && rLight < WHITE_TOL){//turn left
+    
+  }
+
+  if ( lLight < WHITE_TOL && rLight > WHITE_TOL){//turn right
+    
+  }  
+
+  if (debWhite >= DEB_TIME){
+    onWhite = true;
+    changePosFlag = true;
+  }
+  else if(!onWhite && changePosFlag){
+    pos += travelDir;
+    changePosFlag = false;
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

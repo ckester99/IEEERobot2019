4c4368a2f87880c8a04c1796ff696ab4d3bebb05
ckester99 <CKESTER@MURRAYSTATE.EDU>
2020-02-05 20:17:17 -0600

    Add files via upload

--- Arm_Control.ino
+++ Arm_Control.ino
@@ -1,15 +1,22 @@
 #include <Servo.h>
 
 
-#define Xpin 1
-#define Ypin 1
-#define Zpin 1
-#define Ppin 1
-#define Gpin 1
+#define Xpin 3
+#define Ypin 5
+#define Zpin 6
+#define Ppin 9
+#define Gpin 10
 
 Servo Xserv, Yserv, Zserv, Pserv, Gserv;
 float thetaX, thetaY, thetaZ, thetaP, thetaG; // in radians (X is degrees)
 float Ytg, Pte, Offx, L;
+
+void setConstants(){
+  L = 9.26;
+  Ytg = .89;
+  Offx = .54;
+  Pte = 0;
+}
 
 void attchServos(){
   Xserv.attach(Xpin);
@@ -21,22 +28,16 @@ void attchServos(){
 
 void target(float R, float D, float H){
   thetaX = R;
-  thetaZ = abs(2*atan(sqrt((-(D*D - 2*D*Offx - 2*D*Pte + H*H - 2*H*Ytg + Offx*Offx + 2*Offx*Pte + Pte*Pte + Ytg*Ytg)/(D*D - 2*D*Offx - 2*D*Pte + H*H - 2*H*Ytg - 4*L*L + Offx*Offx + 2*Offx*Pte + Pte*Pte + Ytg*Ytg)))));
-  thetaY = asin(L*thetaZ/sqrt(pow(D-Pte,2)+pow(H-Ytg,2)));
-  thetaP = thetaZ+thetaY;
-  Xserv.write(thetaX)
+  float d = D - Pte + Offx;
+  float h = H - Ytg;
+  psiY = atan(h/d);
+  thetaZ = acos(1-(d*d + h*h)/(2*L*L));
+  phiY = asin(L*sin(thetaZ)/sqrt(d*d+h*h));
+  thetaY = phiY+psiY;
+  thetaP = thetaY + thetaZ;
+
+  Xserv.write(R);
   Yserv.write(thetaY*180/M_PI);
   Zserv.write(thetaZ*180/M_PI);
   Pserv.write(thetaP*180/M_PI);
-}
-
-
-void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
 }
